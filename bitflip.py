8a7f3d1451a5241a065e7d5d3d09b2eeb0aec8b9
Dennis Pietarinen <haxxor.p@gmail.com>
2014-09-19 21:19:36 +0200

    Added parser (python3 bitflip.py |column -ts'|')

---
+++ bitflip.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+def main():
+    flist=["manual2001.txt","manual30.txt","utnboken", "upphov"]
+    blist = {}
+    fall = []
+    print("songname|", end='')
+    for f in flist:
+        blist[f] = [l.strip() for l in open(f).readlines()]
+        fall += blist[f]
+        print(f,end='|')
+    print()
+    fall=list(set(fall))
+    fall.sort()
+    for song in fall:
+        print(song, end='')
+        for f in flist:
+            if song in blist[f]:
+                print('|x', end='')
+            else:
+                print('| ', end='')
+        print()
+
+
+
+if __name__ == "__main__":
+    main()
